b5614e596c12bb2d58985eab2fd592eba56c3ef8
Onuraslann <theeaslann1@gmail.com>
2021-10-18 15:06:53 +0300

    Add project files.

---
+++ TCPCLients/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace TCPCLients
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(94, 49);
+            this.txtIP.Multiline = true;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.ReadOnly = true;
+            this.txtIP.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtIP.Size = new System.Drawing.Size(517, 27);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "127.0.0.1:436";
+            this.txtIP.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(527, 380);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(94, 29);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(94, 82);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(517, 246);
+            this.txtInfo.TabIndex = 3;
+            this.txtInfo.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(85, 334);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(526, 27);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(387, 380);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(94, 29);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCP/IP Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ TCPServers/Form1.cs
@@ -0,0 +1,93 @@
+﻿using SimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPServers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        SimpleTcpServer server;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server.Start();
+            txtInfo.Text += $"Starting...{Environment.NewLine}";
+            btnStart.Enabled = false;
+            btnSend.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnSend.Enabled = false;
+            server = new SimpleTcpServer(txtIP.Text);
+            server.Events.ClientConnected += Events_ClientConnected;
+            server.Events.ClientDisconnected += Events_ClientDisconnected;
+            server.Events.DataReceived += Events_DataReceived;
+
+        }
+
+
+        private void Events_DataReceived(object sender, DataReceivedFromClientEventArgs e)
+        {
+           
+            this.Invoke((MethodInvoker)delegate
+               {
+                   txtInfo.Text += $"{e.IpPort}: {Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+               });
+        }
+        private void Events_ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort} disconnected.{Environment.NewLine}";
+                IstClientIP.Items.Remove(e.IpPort);
+            });
+            
+        }
+
+        private void Events_ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort} connected.{Environment.NewLine}";
+                IstClientIP.Items.Add(e.IpPort);
+            });
+
+           
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (server.IsListening)
+            {
+                if (!string.IsNullOrEmpty(txtMessage.Text) && IstClientIP.SelectedItem != null)
+                {
+                    server.Send(IstClientIP.SelectedItem.ToString(), txtMessage.Text);
+                    txtInfo.Text += $"Server:{txtMessage}{Environment.NewLine}";
+                    txtMessage.Text = string.Empty;
+                }
+            }
+        }
+    }
+}
